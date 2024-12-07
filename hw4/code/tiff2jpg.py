bcb134ae8fa78fe99147dcbd2152a292beddeffc
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2024-12-08 01:00:18 +0900

    feat(hw4): rename

---
+++ hw4/code/tiff2jpg.py
@@ -0,0 +1,10 @@
+# ISP PipeLine (RAW image -> JPEG image)
+# White Balancing(Automatic White Balance, AWB) -> CFA Interpolation -> Gamma Correction
+
+import sys
+
+def main():
+    tiff_file_name = sys.argv[1]
+
+if __name__ == "__main__":
+    main()
